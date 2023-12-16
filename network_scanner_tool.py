4c9d570a4d1f3159655f23bc9634abd2891a6db8
jpromanonet <programming@jpromano.net>
2023-12-16 16:16:14 -0300

    Done

--- network_scanner_tool.py
+++ network_scanner_tool.py
@@ -4,7 +4,7 @@ import queue
 import tkinter as tk
 from tkinter import scrolledtext
 
-def scan_ports(target, start_port, end_port, results):
+def scan_ports(target, start_port, end_port, results, loading_var):
     for port in range(start_port, end_port + 1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
@@ -13,15 +13,17 @@ def scan_ports(target, start_port, end_port, results):
             service_name = socket.getservbyport(port)
             results.put((port, service_name))
         sock.close()
+    loading_var.set(False)  # Set loading variable to False when scanning is complete
 
-def worker_thread(target, port_range, results):
+def worker_thread(target, port_range, results, loading_var):
+    loading_var.set(True)  # Set loading variable to True when scanning starts
     while True:
         start_port, end_port = port_range.get()
         if start_port is None:
             break
-        scan_ports(target, start_port, end_port, results)
+        scan_ports(target, start_port, end_port, results, loading_var)
 
-def start_threads(target, num_threads, results):
+def start_threads(target, num_threads, results, loading_var):
     port_ranges = queue.Queue()
     total_ports = 65535
 
@@ -35,7 +37,7 @@ def start_threads(target, num_threads, results):
 
     threads = []
     for _ in range(num_threads):
-        thread = threading.Thread(target=worker_thread, args=(target, port_ranges, results))
+        thread = threading.Thread(target=worker_thread, args=(target, port_ranges, results, loading_var))
         thread.start()
         threads.append(thread)
 
@@ -46,20 +48,38 @@ def start_scan():
     target = target_entry.get()
     num_threads = 4
 
+    loading_var = tk.BooleanVar()
+    loading_var.set(False)
+
+    loading_popup = tk.Toplevel(app)
+    loading_popup.title("Scanning")
+
+    loading_label = tk.Label(loading_popup, text="Scanning in progress...")
+    loading_label.pack(padx=20, pady=20)
+
     results = queue.Queue()
-    start_threads(target, num_threads, results)
+    scan_thread = threading.Thread(target=start_threads, args=(target, num_threads, results, loading_var))
+    scan_thread.start()
 
-    open_ports = []
-    while not results.empty():
-        port, service_name = results.get()
-        open_ports.append((port, service_name))
+    app.after(100, check_loading, loading_popup, loading_var)
 
-    open_ports.sort()
-    result_text.config(state=tk.NORMAL)
-    result_text.delete(1.0, tk.END)
-    for port, service_name in open_ports:
-        result_text.insert(tk.END, f"Port {port} open - Service: {service_name}\n")
-    result_text.config(state=tk.DISABLED)
+def check_loading(loading_popup, loading_var):
+    if loading_var.get():
+        app.after(100, check_loading, loading_popup, loading_var)
+    else:
+        loading_popup.destroy()
+
+        open_ports = []
+        while not results.empty():
+            port, service_name = results.get()
+            open_ports.append((port, service_name))
+
+        open_ports.sort()
+        result_text.config(state=tk.NORMAL)
+        result_text.delete(1.0, tk.END)
+        for port, service_name in open_ports:
+            result_text.insert(tk.END, f"Port {port} open - Service: {service_name}\n")
+        result_text.config(state=tk.DISABLED)
 
 # GUI setup
 app = tk.Tk()
